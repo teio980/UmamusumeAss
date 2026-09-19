e54a2e0987e561fda6b9fe453a89d989b184e7e0
Teio <w.kong70@yahoo.com>
2026-09-19 21:59:05 +0800

    chore: refresh startup build artifacts

--- temp/tasklog_verify_obj/Wpf/Debug/net10.0-windows10.0.17763.0/UmamusumeWpfGui_14rzhrzi_wpftmp.AssemblyInfo.cs
+++ temp/tasklog_verify_obj/Wpf/Debug/net10.0-windows10.0.17763.0/UmamusumeWpfGui_14rzhrzi_wpftmp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UmamusumeAss")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.2.2.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.2.2+4744bd8ca45ee836a4bb5c89e72a29725d93b375")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.2.2+73a96d1750f6bea25930046a113720e6a1d14929")]
 [assembly: System.Reflection.AssemblyProductAttribute("UmamusumeAss")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UmamusumeAss")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.2.2.0")]
